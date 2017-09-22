77832ec9790d563a769b053a871e560668ca6f51
robotecture <31611946+robotecture@users.noreply.github.com>
2017-09-22 04:23:07 -0700

    Responsive Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // Make it rain!!
+$('#menu').on('click', function() {
+    $('.nav-menu').slideToggle(350);
+})
+
+$(window).on('resize', function(){
+      var win = $(this);
+      if (win.width() > 880) {
+      $('.nav-menu').show();
+      }
+});
